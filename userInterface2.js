584cc8e6e3ecace626e747fbac1677f9b2b5bf35
Vanna Sok <vannasok92@yahoo.com>
2019-08-06 14:08:42 -0700

    added codes

---
+++ userInterface2.js
@@ -0,0 +1,74 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+    
+  <title>Great Idea!</title>
+
+  <link href="https://fonts.googleapis.com/css?family=Bangers|Titillium+Web" rel="stylesheet">
+  <link rel="stylesheet" href="css/index.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+  <body>
+    
+    <nav>
+        <a href="#">Services</a>
+        <a href="#">Product</a>
+        <a href="#">Vision</a>
+        <a href="#">Features</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+        <img class="logo" src="img/logo.png" alt="Great Idea! Company logo."></img>
+    </nav>
+<div>    
+    <h1>Innovation<br>On</br>Demand<br></br></h1>
+    <a href="" >Get Started</a>
+    <img src="img/header-img.png" alt="Image of a code snippet."></img>
+</div>
+  
+<section class="content-body">
+    <h3>Features</h3>
+    <p>Aliquam elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>
+
+    <h3>About</h3>
+    <p>Aliquam elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>
+</section>
+<div>
+<   img class="middle-img" src="img/mid-page-accent.jpg" alt="Image of code snippets across the screen">   
+</div>
+<section class="content-button">
+
+    <h3>Services</h3>
+    <p>Aliquam elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>
+  
+    <h3>Product</h3>
+    <p>Aliquam elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>
+
+    <h3>Vision</h3>
+    <p>Aliquam elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.</p>
+</section>
+
+<div>
+    <h3>Contact</h3>
+    <address>
+    123 Way 456 Street<br>
+    Somewhere, USA<br>
+    1 (888) 888-8888<br>
+    sales@greatidea.io
+  
+    </address>
+</div>
+
+<footer>
+    &copy; Copyright Great Idea! 2018
+</footer>
+  
+  
+  
+  </body>
+</html>
